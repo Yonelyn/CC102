1650334493ac1fb583ef9d9b9002b9325c4c30e8
Yonelyn <yonelynyamo1@gmail.com>
2024-12-07 23:10:00 +0800

    Add files via upload

---
+++ Activity_1C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i < 9; i++) {
+		for (int y = 9 - i; y > 0; y--)
+			cout << " ";
+	for (int i = 1; i <= 8; i++) {
+		for (int j = 1; j <= i; j++)
+			cout << " ";
+		for (int y = 9 - i; y > 0; y--)
+			cout << "#";
+		cout << endl;
+	}	
+	return 0;
+}
+}
