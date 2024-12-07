2981a61dedbee28ad5b5963f68fb7ad2c186aa07
Yonelyn <yonelynyamo1@gmail.com>
2024-12-07 21:37:48 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation\n";
+	cout << "College of Coputer Studies\n";
+	cout << "Bachelor of Sciience in Information Technology 1\n";
+	cout << "Yamo,Yonelyn C.\n";
+	cout << "Brgy. Vinas Calauag Quezon\n";
+	
+	return 0;
+}
